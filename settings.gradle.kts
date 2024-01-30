a8204fa62540b98fd6192b472990e4f701722c64
Marioo295 <mramosgaliana@gmail.com>
2024-01-30 12:20:26 +0100

    Cifrado_PA compilado

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,4 +4,4 @@ include ("WSConsultaDoinDocumentos","WSDocumentoElectronico","WSAutenticacionLoc
         "WSNotificaGestionEnvios", "WSServiciosSmartMultas", "WSTrasladoMaquina","WSLiquidacionTasas","WSModelo046",
         "WSFirmasAdes","pa_wsNAutoliquidacionPA","PagoPresentacionModelo600","ArchivoDigital","WSCustodiaImagenes",
         "WSImagenesMultas","CalculoModelo600","ConsultaCertificados","WSAsturconConsultaDeuda","WSAsturconPagosEnte",
-        "WSGestionSubastasBOE")
+        "WSGestionSubastasBOE","Cifrado_PA")
